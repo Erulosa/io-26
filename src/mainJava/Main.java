cd395baf60c79c85afa59ac36a3c2702f0d8cb57
Erumu Samuel <Erumu Samuel>
2015-11-18 13:25:41 +0200

    After frame addition

--- src/mainJava/Main.java
+++ src/mainJava/Main.java
@@ -1,8 +1,14 @@
 package mainJava;
 
+
+import javax.swing.JFrame;
+
 public class Main {
 	public static void main(String[] args){
-		System.out.print("hello world");
+		JFrame frame = new JFrame("My frame");
+	    frame.setLocation(200,400);
+	    frame.setSize(200,400);
+		frame.setVisible(true);
 	}
 
 }
